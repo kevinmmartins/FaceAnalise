44b2dae7f3f547c83d560b7a77c7cdab0dd58155
Kevin Martins <kevinmmartins@gmail.com>
2018-06-10 15:23:07 -0300

    Create connection with rekognition

--- index.py
+++ index.py
@@ -1,13 +1,29 @@
 import boto3
 
 s3= boto3.resource('s3')
+client= boto3.client('rekognition')
 
 def list_images():
     images=[]
     bucket= s3.Bucket('images-tr')
     for image in bucket.objects.all():
         images.append(image.key)
-    print(images)
     return images;
 
-imagens = list_images()
+def create_images_index_list(images):
+    for imageV in images:
+        response=client.index_faces(
+            CollectionId='faces',
+            DetectionAttributes=[
+            ],
+            ExternalImageId=imageV[:-4],
+            Image={
+                'S3Object':{
+                    'Bucket': 'images-t',
+                    'Name':imageV,
+                }
+            }
+        )
+
+imagesFromS3 = list_images()
+create_images_index_list(imagesFromS3)
